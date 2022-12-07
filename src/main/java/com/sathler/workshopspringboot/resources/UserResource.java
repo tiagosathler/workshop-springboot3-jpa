28ecee743582fa8810049839e45fbf5dc571c5c7
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-07 19:56:22 -0300

    :sparkles: feat: Service layer, component registration

--- src/main/java/com/sathler/workshopspringboot/resources/UserResource.java
+++ src/main/java/com/sathler/workshopspringboot/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.sathler.workshopspringboot.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sathler.workshopspringboot.entities.User;
+import com.sathler.workshopspringboot.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999-9999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> users = service.findAll();
+		return ResponseEntity.ok().body(users);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
