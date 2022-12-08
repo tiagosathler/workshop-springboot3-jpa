20e0b8befb0a8f6210334c132e44aa25d114901b
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-08 17:56:57 -0300

    :sparkles: feat: Exception handling - delete

--- src/main/java/com/sathler/workshopspringboot/repositories/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/sathler/workshopspringboot/repositories/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.sathler.workshopspringboot.services.exceptions.DatabaseException;
 import com.sathler.workshopspringboot.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,6 +18,19 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
+
+		StandardError err = new StandardError(
+				Instant.now(),
+				status.value(),
+				"Resource not found",
+				e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 
 		StandardError err = new StandardError(
 				Instant.now(),
