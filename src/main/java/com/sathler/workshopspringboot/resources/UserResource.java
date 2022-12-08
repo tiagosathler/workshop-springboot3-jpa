1c6d32b39a89f91be4bc2613f6a57ce8d906e63e
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-08 16:53:41 -0300

    :sparkles: feat: User delete

--- src/main/java/com/sathler/workshopspringboot/resources/UserResource.java
+++ src/main/java/com/sathler/workshopspringboot/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +35,7 @@ public class UserResource {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User user) {
 		user = service.insert(user);
@@ -44,5 +45,11 @@ public class UserResource {
 				.buildAndExpand(user.getId())
 				.toUri();
 		return ResponseEntity.created(uri).body(user);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
