a61e8607f669b41f85fc36ecac47eb1bfc9c86e3
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-08 18:01:37 -0300

    :sparkles: feat: Exception handling - update

--- src/main/java/com/sathler/workshopspringboot/services/UserService.java
+++ src/main/java/com/sathler/workshopspringboot/services/UserService.java
@@ -13,6 +13,8 @@ import com.sathler.workshopspringboot.repositories.UserRepository;
 import com.sathler.workshopspringboot.services.exceptions.DatabaseException;
 import com.sathler.workshopspringboot.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User user) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, user);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, user);
+			return repository.save(entity);						
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
