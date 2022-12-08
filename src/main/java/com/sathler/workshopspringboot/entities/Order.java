2d5313539b1fe4f534a97cc2c9f104051aafc477
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-08 16:21:41 -0300

    :sparkles: feat: Subtotal & Total methods

--- src/main/java/com/sathler/workshopspringboot/entities/Order.java
+++ src/main/java/com/sathler/workshopspringboot/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
+	public Double getTotal() {
+		Double total = 0.0;
+		for (OrderItem item : items) {
+			total += item.getSubTotal();
+		}
+		return total;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(date, id);
