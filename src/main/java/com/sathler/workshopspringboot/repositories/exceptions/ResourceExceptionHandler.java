b0d6b597c45ed5cee55bbe9ea6f507ab0f86a799
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-08 17:42:54 -0300

    :sparkles: feat: Exception handling - findById

---
+++ src/main/java/com/sathler/workshopspringboot/repositories/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.sathler.workshopspringboot.repositories.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.sathler.workshopspringboot.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+
+		StandardError err = new StandardError(
+				Instant.now(),
+				status.value(),
+				"Resource not found",
+				e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
---
+++ src/main/java/com/sathler/workshopspringboot/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.sathler.workshopspringboot.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
